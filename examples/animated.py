3f8eeb6af37431fe54aad0b0fc01b58c5cb4d057
Jonah Post <48297677+JonahPost@users.noreply.github.com>
2021-04-30 15:56:33 +0200

    fixed some problems with ani again

--- examples/animated.py
+++ examples/animated.py
@@ -9,16 +9,29 @@ if __name__ == "__main__":
         "map_size":50,
         "animals": ["fox", "rabbit"],
         "foods": ["carrot", ],
-        "fox":0,
-        "rabbit":80,
-        "carrot":800,
-        "food_spawn_chance": {"carrot": 0.02},
-        "stop_at_zero":False,
+        "fox":10,
+        "rabbit":200,
+        "carrot":1200,
+        "food_spawn_chance": {"carrot": 0.05},
+        "stop_at_zero":True,
         "animal_std" : 3
     }
 
-    fox_inits = {"mean_speed":5, "mean_reproductive_drive":10, "mean_sight_radius":1, "mean_max_hunger":50}
-    rabbit_inits = {"mean_speed":3, "mean_reproductive_drive":2, "mean_sight_radius":5, "mean_max_hunger":150, "mean_max_age":100}
+    fox_inits = {
+        "mean_speed":3,
+        "mean_reproductive_drive":10,
+        "mean_sight_radius":1,
+        "mean_max_hunger":120,
+        "mean_max_age":300
+    }
+    rabbit_inits = {
+        "mean_speed":3,
+        "mean_reproductive_drive":5,
+        "mean_sight_radius":1,
+        "mean_max_hunger":60,
+        "mean_max_age":100,
+        "nutritional_value":100
+    }
 
     animal_objects = {"fox": {"object":Fox, "init":fox_inits}, "rabbit": {"object":Rabbit, "init":rabbit_inits}}
     food_objects = {"carrot": Carrot}
--- examples/main.py
+++ examples/main.py
@@ -4,33 +4,33 @@ from src.visualization import map_graph, population_stats_plot, animal_stats_plo
 import numpy as np
 
 if __name__ == "__main__":
-	settings = {
-		"map_size":50,
-		"animals": ["fox", "rabbit"],
-		"foods": ["carrot", ],
-		"fox":10,
-		"rabbit":200,
-		"carrot":1200,
-		"food_spawn_chance": {"carrot": 0.05},
-		"stop_at_zero":True,
-		"animal_std" : 3
-	}
+    settings = {
+        "map_size":50,
+        "animals": ["fox", "rabbit"],
+        "foods": ["carrot", ],
+        "fox":10,
+        "rabbit":200,
+        "carrot":1200,
+        "food_spawn_chance": {"carrot": 0.05},
+        "stop_at_zero":True,
+        "animal_std" : 3
+    }
 
-	fox_inits = {
-		"mean_speed":3,
-		"mean_reproductive_drive":10,
-		"mean_sight_radius":1,
-		"mean_max_hunger":120,
-		"mean_max_age":300
-	}
-	rabbit_inits = {
-		"mean_speed":3,
-		"mean_reproductive_drive":5,
-		"mean_sight_radius":1,
-		"mean_max_hunger":60,
-		"mean_max_age":100,
-		"nutritional_value":100
-	}
+    fox_inits = {
+        "mean_speed":3,
+        "mean_reproductive_drive":10,
+        "mean_sight_radius":1,
+        "mean_max_hunger":120,
+        "mean_max_age":300
+    }
+    rabbit_inits = {
+        "mean_speed":3,
+        "mean_reproductive_drive":5,
+        "mean_sight_radius":1,
+        "mean_max_hunger":60,
+        "mean_max_age":100,
+        "nutritional_value":100
+    }
 
     animal_objects = {"fox": {"object":Fox, "init":fox_inits}, "rabbit": {"object":Rabbit, "init":rabbit_inits}}
     food_objects = {"carrot": Carrot}
@@ -45,17 +45,17 @@ if __name__ == "__main__":
     #    stats = np.append(stats,ae.run_cycles(maxcycles=3), axis=0)
         #map_graph(ae.printable_map())
 
-	stats, genes = ae.run_cycles(maxcycles=100)
-	#map_graph(ae.printable_map())
-	#for i in range(7):
-	#	stats = np.append(stats,ae.run_cycles(maxcycles=3), axis=0)
-		#map_graph(ae.printable_map())
+    stats, genes = ae.run_cycles(maxcycles=100)
+    #map_graph(ae.printable_map())
+    #for i in range(7):
+    #    stats = np.append(stats,ae.run_cycles(maxcycles=3), axis=0)
+        #map_graph(ae.printable_map())
 
-	#stats = np.append(stats,ae.run_cycles(maxcycles=1000), axis=0)
-	#map_graph(ae.printable_map())
+    #stats = np.append(stats,ae.run_cycles(maxcycles=1000), axis=0)
+    #map_graph(ae.printable_map())
 
-	population_stats_plot(stats, food_objects, animal_objects)
+    population_stats_plot(stats, food_objects, animal_objects)
 
-	for i, animal in enumerate(animal_objects.keys()):
-		animal_stats_plot(genes[::, i, ::, ::], title=animal, labels=(0, 3))
-		animal_stats_plot(genes[::, i, ::, ::], title=animal, labels=(3, 8))
+    for i, animal in enumerate(animal_objects.keys()):
+        animal_stats_plot(genes[::, i, ::, ::], title=animal, labels=(0, 3))
+        animal_stats_plot(genes[::, i, ::, ::], title=animal, labels=(3, 8))
