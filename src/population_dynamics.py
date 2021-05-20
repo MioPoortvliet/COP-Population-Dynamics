0a372651b5fc198fe43f8dab03883dfe07b69489
Mio <miodragpoortvliet@gmail.com>
2021-05-20 18:20:12 +0200

    wel een +1

--- src/population_dynamics.py
+++ src/population_dynamics.py
@@ -336,7 +336,7 @@ class AnimalEvolution:
 			self.cycle()
 			self.write_stats(cycle)
 
-		return self.population[:cycle, ::], self.animal_genes[:cycle, ::, ::, ::]
+		return self.population[:cycle+1, ::], self.animal_genes[:cycle+1, ::, ::, ::]
 
 	def delete_entity(self, entity, map:np.ndarray) -> None:
 		"""Remove entity from the map and flag it to not be interacted with."""
